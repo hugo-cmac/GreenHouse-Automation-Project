59902cf3acb8f272d54b2a9a19acb4c395a6719d
pedrorocha30 <pedroar94@gmail.com>
2021-01-07 23:09:37 +0000

    logout e correcções

--- RESTAPI/frontend/admin.js
+++ RESTAPI/frontend/admin.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     
-    
-    
-    
-
-
+    $.ajax({
+		url: "localhost:3000/" + "files",
+		type: 'GET',
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		data: {},
+		success: function (data) {
+			edit=localStorage.getItem("userId");  
+			document.getElementById('user').innerHTML = edit;
+			
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+			// alert(xhr.status);
+			// alert(thrownError); 
+		}
+	});   
 });
