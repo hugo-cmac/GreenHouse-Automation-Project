1ea49b44a1bba11e2674f577daa411fc02b15428
pedrorocha30 <pedroar94@gmail.com>
2021-01-10 02:13:43 +0000

    geral-perfil adicionado (falta ativar perfil)

--- RESTAPI/frontend/geral-perfil.js
+++ RESTAPI/frontend/geral-perfil.js
@@ -17,10 +17,35 @@ $(document).ready(function(){
         crossDomain:true,
         data:{},
         success:function(data){
-            /*console.log(data.data.username);
-            localStorage.setItem('userID',data.data.username);*/
+            console.log(data.data.length);
+            /*localStorage.setItem('userID',data.data.username);*/
             //localStorage.setItem('userIdgrande',data.userIdgrande);
             //alert("Perfil adiconado!\nA redirecionar...")
+            for(i=0;i<data.data.length;i++){
+                $('#listaPerfil').append(
+                    "<div class=\"card\">"+
+                        "<div class=\"card-header\">" +
+                            "<h5 class=\"mb-0\">" +
+                                "<button class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseOne\" aria-expanded=\"true\" aria-controls=\"collapseOne\">" +
+                                    "Perfil nº: " + data.data[i].id_perfil +
+                            "</h5>"+
+                        "</div>"+
+                        "<div id=\"collapseOne\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"+
+                            "<div class=\"card-body\">"+
+                                
+                                    "Designação: "+data.data[i].designacao+"<br/>"+
+                                    "Temperatura minima: "+data.data[i].temp_min+"<br/>"+
+                                    "Temperatura máxima: "+data.data[i].temp_max+"<br/>"+
+                                    "Humidade do ar mínima: "+data.data[i].hum_air_min+"</br/>"+
+                                    "Humidade do ar máxima: "+data.data[i].hum_air_max+"<br/>"+
+                                    "Humidade do solo mínima: "+data.data[i].hum_earth_min+"<br/>"+
+                                    "Humidade do solo máxima: "+data.data[i].hum_earth_max+"<br/>"+ 
+                                    "<a class=\"banner_btn\" id=\"active_btn_"+i+ "value=\"Ativar\">Ativar<i class=\"ti-arrow-right\"></i></a>"+               
+                            "</div>"+
+                        "</div>"+
+                    "</div>"
+                );
+            }
         },
         error: function (xhr, ajaxOptions, thrownError) {
             console.log(JSON.stringify(xhr));
