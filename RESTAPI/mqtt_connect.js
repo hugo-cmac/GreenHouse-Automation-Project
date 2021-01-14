5784da78aa55028fec58d47856caf935449db54b
Augusto Mota <augustocesarsilvamota@gmail.com>
2021-01-14 03:13:20 +0000

    Update mqtt_connect.js

--- RESTAPI/mqtt_connect.js
+++ RESTAPI/mqtt_connect.js
@@ -1,41 +1,32 @@
-var hostname = "mqtt.dioty.co";
-var port = 1883;
-var clientId = "CERCI_ESP_ID";
-clientId += new Date().getUTCMilliseconds();;
-var username = "augustocesarsilvamota@gmail.com";
-var password = "323c0782";
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDAMQP_MQTT_URL || 'mqtt://localhost:1883');
+var auth = (mqtt_url.auth || ':').split(':');
+var url = "mqtt://" + mqtt_url.host;
 
-mqttClient = new Paho.MQTT.Client(hostname, port, clientId);
-mqttClient.onMessageArrived = MessageArrived;
-mqttClient.onConnectionLost = ConnectionLost;
-Connect();
+//username: auth[0] + ":" + auth[0] if you are on a shared instance
+var options = {
+  port: mqtt_url.port,
+  clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+  username: auth[0],
+  password: auth[1],
+};
 
-/*Initiates a connection to the MQTT broker*/
-function Connect(){
-	mqttClient.connect({
-	onSuccess: Connected,
-	onFailure: ConnectionFailed,
-	keepAliveInterval: 10,
-	userName: username,
-	useSSL: true,
-	password: password});
-}
+// Create a client connection
+var client = mqtt.connect(url, options);
 
-/*Callback for successful MQTT connection */
-function Connected() {
-	console.log("Connected");
-	mqttClient.subscribe(subscription);
-}
+client.on('connect', function() { // When connected
 
-/*Callback for failed connection*/
-function ConnectionFailed(res) {
-	console.log("Connect failed:" + res.errorMessage);
-}
+  // subscribe to a topic
+  client.subscribe('hello/world', function() {
+    // when a message arrives, do something with it
+    client.on('message', function(topic, message, packet) {
+      console.log("Received '" + message + "' on '" + topic + "'");
+    });
+  });
 
-/*Callback for lost connection*/
-function ConnectionLost(res) {
-	if (res.errorCode !== 0) {
-		console.log("Connection lost:" + res.errorMessage);
-		Connect();
-	}
-}
+  // publish a message to a topic
+  client.publish('hello/world', 'my message', function() {
+    console.log("Message is published");
+    client.end(); // Close the connection when published
+  });
+});
