c2083ba2d110938ce9584607d004095eb203b651
pedrorocha30 <pedroar94@gmail.com>
2021-01-14 23:15:58 +0000

    perfis com butoes dinamicos e fonte mudada

--- RESTAPI/frontend/geral-perfil.js
+++ RESTAPI/frontend/geral-perfil.js
@@ -1,6 +1,7 @@
 var edit;
 var aux=[];
-var cont =0;
+var cont = 0;
+var contb = 0;
 
 var url = "ws://mqtt.dioty.co:8080/mqtt";
 var options = {
@@ -44,31 +45,37 @@ $(document).ready(function(){
                 //console.log(aux[i]);
                 aux[i]=data.data[i];
                 cont = data.data.length;
-                $('#listaPerfil').append(
-                    "<div class=\"card\">"+
-                        "<div class=\"card-header\">" +
-                            "<h5 class=\"mb-0\">" +
-                                "<button class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseOne\" aria-expanded=\"true\" aria-controls=\"collapseOne\">" +
-                                    "Perfil nº: " + data.data[i].id_perfil +
-                            "</h5>"+
-                        "</div>"+
-                        "<div id=\"collapseOne\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"+
-                            "<div class=\"card-body\">"+
-                                    "Designação: "+data.data[i].designacao+"<br/>"+
-                                    "Temperatura minima: "+data.data[i].temp_min+"<br/>"+
-                                    "Temperatura máxima: "+data.data[i].temp_max+"<br/>"+
-                                    "Humidade do ar mínima: "+data.data[i].hum_air_min+"</br/>"+
-                                    "Humidade do ar máxima: "+data.data[i].hum_air_max+"<br/>"+
-                                    "Humidade do solo mínima: "+data.data[i].hum_earth_min+"<br/>"+
-                                    "Humidade do solo máxima: "+data.data[i].hum_earth_max+"<br/>"+ 
-                                    "<a class=\"banner_btn\" id=\"active_btn_"+i+"\""+" "+ "value=\"Ativar\">Ativar<i class=\"ti-arrow-right\"></i></a>"+               
+                $('#listaPerfil2').append(
+                    "<div class=\"col-sm-12 col-lg-6\">" +
+                        "<div class=\"accordion\" id=\"listaPerfil\">"+
+                            "<div class=\"card\">"+
+                                "<div class=\"card-header\">" +
+                                    "<h5 class=\"mb-0\">" +
+                                        "<button class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseOne\" aria-expanded=\"true\" aria-controls=\"collapseOne\">" +
+                                            "Perfil nº: " + data.data[i].id_perfil +
+                                    "</h5>"+
+                                "</div>"+
+                                "<div id=\"collapseOne\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"+
+                                    "<div class=\"card-body\">"+
+                                            "Designação: "+data.data[i].designacao+"<br/>"+
+                                            "Temperatura minima: "+data.data[i].temp_min+"<br/>"+
+                                            "Temperatura máxima: "+data.data[i].temp_max+"<br/>"+
+                                            "Humidade do ar mínima: "+data.data[i].hum_air_min+"</br/>"+
+                                            "Humidade do ar máxima: "+data.data[i].hum_air_max+"<br/>"+
+                                            "Humidade do solo mínima: "+data.data[i].hum_earth_min+"<br/>"+
+                                            "Humidade do solo máxima: "+data.data[i].hum_earth_max+"<br/>"+ 
+                                            "<a class=\"banner_btn\" id=\"active_btn_"+i+"\""+" "+ "value=\"Ativar\">Ativar<i class=\"ti-arrow-right\"></i></a>"+               
+                                    "</div>"+
+                                "</div>"+
                             "</div>"+
                         "</div>"+
                     "</div>"
                 );
+
+
             }
 
-            for(var a=0;a<cont;a++){
+            /*for(var a=0;a<cont;a++){
                 console.log(aux[a]);
                 
                 $("#active_btn_"+a).click(function() {
@@ -78,7 +85,56 @@ $(document).ready(function(){
                         client.end(); // Close the connection when published
                     });
                 });
-            }
+            }*/
+
+            $.ajax({
+                url:  localStorage.getItem('base_url')+"devices",
+                type: 'GET',
+                dataType : 'json',
+                contentType: "application/json; charset=utf-8",
+                crossDomain:true,
+                data:{},
+                success:function(data){
+                    console.log(data.data.length);
+                    contb = data.data.length;
+                    /*localStorage.setItem('userID',data.data.username);*/
+                    //localStorage.setItem('userIdgrande',data.userIdgrande);
+                    //alert("Perfil adiconado!\nA redirecionar...")
+                    for(i=0;i<data.data.length;i++){
+                        $('#swiPerfil').append(
+                            "<div class=\"switch-wrap d-flex justify-content-between\">"+
+                                "<p>"+ "Dispositivo " +(i+1)+ " (SN)" +":  " + data.data[i].serial_number +"</p>"+
+                                "<div class=\"primary-switch\">"+
+                                "<input type=\"checkbox\""+ "  value=\""+ data.data[i].serial_number +"\""+ " " + "id=\"default-switch"+i+"\""+" "+"checked" +">"+
+                                "<label for=\"default-switch"+i+"\""+">"+"</label>"+
+                                "</div>"+
+                            "</div>"
+                        );
+                    }
+
+                    for(var a=0;a<cont;a++){
+                        console.log(aux[a]);
+                        $("#active_btn_"+a).click(function() {
+                            for(var b=0;b<contb;b++){
+                                if(document.getElementById("default-switch"+b).checked == true){
+                                    console.log(aux[a]);
+                                    var val = document.getElementById("default-switch"+b).value;
+                                    var payload = "000000;2;"+aux[a].temp_max+";"+aux[a].temp_min+";"+aux[a].hum_air_max+";"+aux[a].hum_air_min+";"+aux[a].hum_earth_max+";"+aux[a].hum_earth_min;
+                                    client.publish('/augustocesarsilvamota@gmail.com/'+val, payload, function() {
+                                        console.log("Message is published");
+                                        client.end(); // Close the connection when published
+                                    });
+                                }
+                            }
+                        });
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(JSON.stringify(xhr));
+                    console.log(JSON.stringify(thrownError));
+                    alert("Inseriu valores não válidos !");
+                }
+            });
 
         },
         error: function (xhr, ajaxOptions, thrownError) {
