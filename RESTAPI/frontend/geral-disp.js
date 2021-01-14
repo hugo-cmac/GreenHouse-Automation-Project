f035036ca56932a86785d8c3ab247144fa821872
pedrorocha30 <pedroar94@gmail.com>
2021-01-14 23:45:09 +0000

    update frontend

--- RESTAPI/frontend/geral-disp.js
+++ RESTAPI/frontend/geral-disp.js
@@ -1,5 +1,6 @@
 var edit;
 var aux =0; 
+var aux2=[];
 
 $(document).ready(function(){
     jQuery.support.cors = true;
@@ -14,24 +15,26 @@ $(document).ready(function(){
         data:{},
         success:function(data){
             console.log(data.data.length);
-            /*localStorage.setItem('userID',data.data.username);*/
-            //localStorage.setItem('userIdgrande',data.userIdgrande);
-            //alert("Perfil adiconado!\nA redirecionar...")
             for(i=0;i<data.data.length;i++){
+                aux2[i]=data.data[i];
                 aux++;
-                $('#listaPerfil').append(
-                    "<div class=\"card\">"+
-                        "<div class=\"card-header\">" +
-                            "<h5 class=\"mb-0\">" +
-                                "<button class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseOne\" aria-expanded=\"true\" aria-controls=\"collapseOne\">" +
-                                    "Dispositivo nº: " + aux +
-                            "</h5>"+
-                        "</div>"+
-                        "<div id=\"collapseOne\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"+
-                            "<div class=\"card-body\">"+
-                                
-                                    "Serial Number (SN): "+data.data[i].serial_number+"<br/>"+
-                                   
+                $('#listaDisp').append(
+                    "<div class=\"col-sm-12 col-lg-6\">" +
+                        "<div class=\"accordion\" id=\"listaPerfil\">"+
+                            "<div class=\"card\">"+
+                                "<div class=\"card-header\">" +
+                                    "<h5 class=\"mb-0\">" +
+                                        "<button class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseOne\" aria-expanded=\"true\" aria-controls=\"collapseOne\">" +
+                                            "Dispositivo nº: " + aux +
+                                    "</h5>"+
+                                "</div>"+
+                                "<div id=\"collapseOne\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"+
+                                    "<div class=\"card-body\">"+
+                                        
+                                            "Serial Number (SN): "+data.data[i].serial_number+"<br/>"+
+                                        
+                                    "</div>"+
+                                "</div>"+
                             "</div>"+
                         "</div>"+
                     "</div>"
