d38420a674ad00685306ac1f9eeaa15a59e82210
Augusto Mota <augustocesarsilvamota@gmail.com>
2021-01-14 00:39:28 +0000

    ui

--- RESTAPI/mqtt.js
+++ RESTAPI/mqtt.js
@@ -1,4 +1,33 @@
-var net = require('net')
-var mqttCon = require('mqtt-connection')
-var stream = net.createConnection(1883, 'localhost')
-var conn = mqttCon(stream)
+import mqtt from 'mqtt'
+
+// connection option
+const options = {
+  		clean: true, // retain session
+      connectTimeout: 4000, // Timeout period
+      // Authentication information
+      clientId: 'emqx_test',
+      username: 'emqx_test',
+      password: 'emqx_test',
+}
+
+// Connect string, and specify the connection method by the protocol
+// ws Unencrypted WebSocket connection
+// wss Encrypted WebSocket connection
+// mqtt Unencrypted TCP connection
+// mqtts Encrypted TCP connection
+// wxs WeChat applet connection
+// alis Alipay applet connection
+const connectUrl = 'wss://broker.emqx.io:8084/mqtt'
+const client = mqtt.connect(connectUrl, options)
+
+client.on('reconnect', (error) => {
+    console.log('reconnecting:', error)
+})
+
+client.on('error', (error) => {
+    console.log('Connection failed:', error)
+})
+
+client.on('message', (topic, message) => {
+  console.log('receive message：', topic, message.toString())
+})
