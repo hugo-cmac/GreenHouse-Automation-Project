381002da567e4af9500524b2137d6f197d98613b
Augusto Mota <augustocesarsilvamota@gmail.com>
2021-01-14 01:49:17 +0000

    mqtt_connect.js

---
+++ RESTAPI/mqtt_connect.js
@@ -0,0 +1,25 @@
+var mqtt = require(mqtt);
+
+// Create a client connection
+var client = mqtt.connect("mqtt://mqtt.dioty.co:1883", {
+username: "augustocesarsilvamota@gmail.com",
+password: "323c0782"
+});
+
+client.on('connect', function() { // Check you have a connection
+/*
+// Subscribe to a Topic
+client.subscribe(/yourRootTopic/#, function() {
+// When a message arrives, write it to the console
+        client.on('message', function(topic, message, packet) {
+            console.log("Received '" + message + "' on '" + topic + "'");
+        });
+    });
+
+// Publish a message to a Topic
+    client.publish(/yourRootTopic/test, 'Hello World Message!', function() {
+        console.log("Message posted...");
+        client.end(); // Close the connection after publish
+    });
+    */
+});
