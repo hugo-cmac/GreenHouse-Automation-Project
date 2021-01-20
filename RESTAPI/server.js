706fc5b9002d0a04ae63ed0dd5b56e6d748a1326
pedrorocha30 <pedroar94@gmail.com>
2021-01-20 02:42:24 +0000

    https incompleto

--- RESTAPI/server.js
+++ RESTAPI/server.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
 const http = require('http');
+const https = require('https');
 const app = require('./app');
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var privateKey  = fs.readFileSync('./Cert/cert.key', 'utf8');
+var certificate = fs.readFileSync('./Cert/cert.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
@@ -9,7 +15,7 @@ connect().use(serveStatic(__dirname)).listen(8080, function(){
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = https.createServer(credentials,app);
 
 server.listen(port);
 
