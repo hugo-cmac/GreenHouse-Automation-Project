b224b5c619e575aa6ff0d1ca8417f166c4cf7019
pedrorocha30 <pedroar94@gmail.com>
2021-01-21 11:42:56 +0000

    update query

--- RESTAPI/api/servBD.js
+++ RESTAPI/api/servBD.js
@@ -482,7 +482,7 @@ module.exports={
 		});
 	},
 	getUSRDEVById: function (req, callback) {
-		let query = "SELECT rud.id_rel_user_device, rud.id_user,d.serial_number,d.designacao FROM Device d join rel_user_device rud on rud.serial_nuber=d.serial_number WHERE rud.id_user = ?";
+		let query = "SELECT rud.id_rel_user_device, rud.id_user,rud.serial_number,rud.designacao FROM Device d join rel_user_device rud on rud.serial_number=d.serial_number WHERE rud.id_user = ?";
 		let table = [req.params.id_user];
 		query = mysql.format(query, table);
 		pool.query(query, function (error, results) {
